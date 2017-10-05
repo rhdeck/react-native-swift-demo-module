cca3f0f2c1d709f35da1d7675e2e4d72fcc3dc1b
Ray Deck <ray@raydeck.com>
2017-10-05 14:55:49 -0400

    Cleaning up core include

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ import {
 } from 'react-native';
 export default class SwiftTest {
   testSend(a, b) {
-console.log(NativeModules); 
-console.log("Hello"); 
+    console.log("Hello"); 
     console.log(
-  "Constants available immediately: ", 
-  NativeModules.stest.a, 
-  NativeModules.stest.b
-);
-const e = new NativeEventEmitter(NativeModules.stest);
-const s = e.addListener("Hello", (arr) => {
-  console.log("Received a hello event", arr.message);
-})
-
+      "Constants available immediately: ", 
+      NativeModules.stest.a, 
+      NativeModules.stest.b
+    );
+    const e = new NativeEventEmitter(NativeModules.stest);
+    const s = e.addListener("Hello", (arr) => {
+      console.log("Received a hello event", arr.message);
+    })
     return NativeModules.stest.addEvent(a, b)
-
   }
-
 }
